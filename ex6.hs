b681c4e93477b759b34231b936c853218512e179
LG West <larsgwest@gmail.com>
2021-08-23 20:40:40 +0200

    ex6 wip

--- ex6.hs
+++ ex6.hs
@@ -11,4 +11,25 @@ sumdown :: Int -> Int
 sumdown 0 = 0
 sumdown n = n + sumdown(n-1)
 
--- 3
+-- 3 defin exponentiation ^
+--(^) :: Int -> Int -> Int
+--(^) n 0 = 1
+--(^) n m = n * pow m-1
+
+-- 4 euclid
+euclid :: Int -> Int -> Int
+euclid n m | n == m  = n
+           | n < m   = euclid n (m-n)
+           | n > m   = euclid (n-m) m
+
+-- 5
+
+-- 6a 
+and :: [Bool] -> Bool
+and (False:_) = False
+and (b:bs) | b == True = and bs
+
+-- 6b 
+concat :: [[a]] ->[a]
+
+-- 6c
